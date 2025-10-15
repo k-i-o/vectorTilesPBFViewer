aab4cbf344d0b275407aeb85787062ae9539c518
k-i-o <sradici26@gmail.com>
2025-10-15 09:41:30 +0200

    test fix pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/vectorTilesPBFViewer/' : '/',
+  base: './',
   server: {
     port: 3000,
     open: true
